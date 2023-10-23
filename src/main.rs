ca00358a7183095147da65a98f132d205635e4b9
Santiago Peraza <perazasantiago93@gmail.com>
2023-10-23 10:28:31 -0300

    Added error handling for invalid ci format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -6,38 +8,70 @@ struct CliArgs {
     option: char,
 }
 
+#[derive(Debug)]
+pub enum CIError {
+    /// Represents an error while validating a CI
+    ValidateInvalidFormat(String),
+    /// Represents an error while generating the last digit of a CI
+    GenerateLastDigitInvalidFormat(String),
+}
+
+impl fmt::Display for CIError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let message = match self {
+            CIError::ValidateInvalidFormat(ci) => format!("Invalid validation format, the CI must be 8 digits: {}", ci),
+            CIError::GenerateLastDigitInvalidFormat(ci) => format!("Invalid last digit generation format, the CI must be 7 digits: {}", ci)
+        };
+        write!(f, "{}", message)
+    }
+}
+
 fn main() {
     let CliArgs { ci, option } = CliArgs::parse();
-
     if option == 'v' {
-        validate(ci)
+        let is_valid = validate(ci).map_err(|_e| CIError::ValidateInvalidFormat(ci.to_string()));
+        match is_valid {
+            Ok(true) => println!("✅ The CI {} is valid", ci),
+            Ok(false) => println!("❌ The CI {} is not valid", ci),
+            Err(e) => println!("{}", e)
+        }
     } else if option == 'g' {
-        generate(ci)
+        let last_digit = generate_last_digit(ci).map_err(|_e| CIError::GenerateLastDigitInvalidFormat(ci.to_string()));
+        
+        match last_digit {
+            Ok(value) => println!("The generated last digit is: {}, the complete CI is: {}{}", value, ci, value),
+            Err(e) => println!("{}", e)
+        }
     } else {
         panic!("Invalid option {:?}", option)
     }
 }
 
-fn validate(ci: u32) {
-    println!("Validating CI {}", ci);
+fn validate(ci: u32) -> Result<bool, CIError> {
     let digits = ci.to_string().len();
     if digits != 8 {
-        panic!("Invalid CI format");
+        Err(CIError::ValidateInvalidFormat(ci.to_string()))
     } else {
+        println!("Validating CI {}", ci);
         let expected_last_digit = calculate_last_digit(ci);
         let last_digit = ci.to_string().chars().last().unwrap().to_digit(10).unwrap();
         if last_digit == expected_last_digit {
-            println!("✅ The CI {} is valid", ci)
+            Ok(true)
         } else {
-            println!("❌ The CI {} is not valid", ci)
+            Ok(false)
         }
     }
 }
 
-fn generate(ci: u32) {
-    println!("Generating last digit for CI: {}", ci);
-    let last_digit = calculate_last_digit(ci);
-    println!("The generated last digit is: {}, the complete CI is: {}{}", last_digit, ci, last_digit);
+fn generate_last_digit(ci: u32) -> Result<u32, CIError> {
+    let digits = ci.to_string().len();
+    if digits != 7 {
+        Err(CIError::GenerateLastDigitInvalidFormat(ci.to_string()))
+    } else {
+        println!("Generating last digit for CI: {}", ci);
+        let last_digit = calculate_last_digit(ci);
+        Ok(last_digit)
+    }
 }
 
 fn calculate_last_digit(ci: u32) -> u32 {
@@ -46,7 +80,6 @@ fn calculate_last_digit(ci: u32) -> u32 {
 
     let validators = vec![8, 1, 2, 3, 4, 7, 6];
     let mut sum = 0;
-    println!("Validating ci {}", ci);
     for (index, ele) in digits.iter().enumerate() {
         sum += validators[index] * ele
     }
